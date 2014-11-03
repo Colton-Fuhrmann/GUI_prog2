6c29c44bb9debc8cfe66c472a5e880cdba5d8767
Hilt <7106215@linux15.mcs.sdsmt.edu>
2014-11-03 16:30:22 -0700

    Added basic NetBeans project

---
+++ StarMap/src/starmap/StarMap.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package starmap;
+
+/**
+ *
+ * @author 7106215
+ */
+public class StarMap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("test\n");
+    }
+}
